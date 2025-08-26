d454dbdff4166bb33919533ab2cf56b5dbeffa0e
RMDcharuka <charukadinal6969@gmail.com>
2025-08-26 10:18:26 +0530

    Update app port to 8083 and fix route

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-'use strict';
+''use strict';
 
-const express = require('express')
+const express = require('express');
 const app = express();
 
-const port = 8080;
+const port = 8083;           // changed from 8080 → 8083
 const host = '0.0.0.0';
 
 app.get('/', (req, res) => {
-  res.send('Hello World from IBM Cloud Essentials!');
-  req.send('testing pipeline');
-})
+  res.send('Hello World my app.js from IBM Cloud Essentials! testing complete');
+});
 
-app.listen(port, host);
-console.log(`Running on http://${host}:${port}`);
+app.listen(port, host, () => {
+  console.log(`Running on http://${host}:${port}`);
+});
+
