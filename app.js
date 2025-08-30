6b5ca33befc2521e729e641a5cb1334054ea1cad
RMDcharuka <charukadinal6969@gmail.com>
2025-08-30 16:34:28 +0530

    changing app.js to check pipeline is working

--- app.js
+++ app.js
@@ -3,14 +3,15 @@
 const express = require('express');
 const app = express();
 
-const port = 8083;           // changed from 8080 → 8083
+const port = 8083;
 const host = '0.0.0.0';
 
 app.get('/', (req, res) => {
-  res.send('Hello World my app.js from IBM Cloud Essentials! testing complete');
+  res.send('successfully deploy demo application');  // <-- updated message
 });
 
 app.listen(port, host, () => {
   console.log(`Running on http://${host}:${port}`);
 });
+
 
